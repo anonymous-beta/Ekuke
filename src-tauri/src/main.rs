fa623a9195464f606c6fa28e20e5529697aec228
Anonymous 🎭 <157519745+anonymous-beta@users.noreply.github.com>
2026-09-17 09:04:24 +0100

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-pub mod cmd;
-pub mod search;
-pub mod models;
-pub mod utils;
-
 fn main() {
-    // Initialize the custom commands plugin
-    let commands_plugin = cmd::init();
-
-    tauri::Builder::default()
-        // Manage the AppState so it can be injected into commands
-        .manage(cmd::AppState {
-            global_config: tokio::sync::Mutex::new(serde_json::json!({})),
-            db: tokio::sync::Mutex::new(None),
-            db_path: tokio::sync::Mutex::new(None),
-            search: tokio::sync::Mutex::new(None),
-        })
-        .plugin(commands_plugin)
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    ekuke::run();
 }
